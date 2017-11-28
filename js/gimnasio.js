ab3e4e6163ac4df118996bdf9f5fd263edbdff83
andreacuellar2 <cuellarandrea10@gmail.com>
2017-11-28 14:02:28 -0600

    Autocomplementar

--- js/gimnasio.js
+++ js/gimnasio.js
@@ -1,3 +1,4 @@
+//Cargar Sucursales
 function cargarSucursal() {
     $.ajax({
         url: 'http://localhost/parcialFPI_4_Servidor/index.php/welcome/getSucursal',
@@ -9,6 +10,33 @@ function cargarSucursal() {
         }
         console.log(response);
       });
+}
+//para autocomplementar los usuarios para eliminar y editar
+var usercomply = new Array();
+window.onload = function() {
+  $.ajax({
+      url: 'http://localhost/parcialFPI_4_Servidor/index.php/welcome/getCliente',
+      dataType: 'json',
+      type: 'GET',
+  }).done(function (response) {
+      for (i = 1; i < response.length; i++) {
+      usercomply[i] = String(response[i].nombre_cliente);
+    }
+    console.log(usercomply);
+    creadorDeListasClientes(usercomply);
+  });
+}
+
+function creadorDeListasClientes(usercomply) {
+  for (i = 0; i <usercomply.length ; i++) {
+    var node = document.createElement("OPTION");
+    var textnode = document.createTextNode(usercomply[i]);
+    node.appendChild(textnode);
+    node.setAttribute("id", i);
+    node.setAttribute("name", usercomply[i]);
+    document.getElementById("lista").appendChild(node);
+    document.getElementById("list").appendChild(node);
+  }
 }
 //funciones para div de clientes
 function ocultarDivs(){
